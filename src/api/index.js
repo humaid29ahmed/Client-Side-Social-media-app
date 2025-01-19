032a467704f2904f87999dd952053d35b181b4a1
humaid29ahmed <humaid29masood@gmail.com>
2025-01-19 19:12:09 +0530

    just changed the baseURL in api folder index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL:'http://localhost:5000'});
+const API = axios.create({baseURL:'https://client-side-social-media-app.onrender.com'});
 
 
 /* Before any request this interceptor sets the req.headers.Authorization 
